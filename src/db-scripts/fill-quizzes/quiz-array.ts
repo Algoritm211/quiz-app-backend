daefea2cb357a8d69c7974aea5bba7cac5cec899
Alexey <algoritm211@gmail.com>
2024-10-05 18:12:29 +0300

    feat: add two quizzes to the array

--- src/db-scripts/fill-quizzes/quiz-array.ts
+++ src/db-scripts/fill-quizzes/quiz-array.ts
@@ -82,6 +82,63 @@ console.log(freddie.colorChange('orange'));
   In this example, JavaScript converts the number 1 into a string, in order for the function to make sense and return a value. During the addition of a numeric type (1) and a string type ('2'), the number is treated as a string. We can concatenate strings like "Hello" + "World", so what's happening here is "1" + "2" which returns "12".
       `,
       },
+      {
+        text: 'What`s the output?',
+        codeSnippet: `
+  \`\`\`js
+  const bird = {
+    size: 'small',
+  };
+  
+  const mouse = {
+    name: 'Mickey',
+    small: true,
+  };
+  \`\`\`
+      `,
+        options: [
+          { text: 'mouse.bird.size is not valid', isCorrect: true },
+          { text: 'mouse[bird.size] is not valid', isCorrect: false },
+          { text: 'mouse[bird["size"]] is not valid', isCorrect: false },
+          { text: 'All of them are valid', isCorrect: false },
+        ],
+        explanation: `
+        In JavaScript, all object keys are strings (unless it's a Symbol). Even though we might not _type_ them as strings, they are always converted into strings under the hood.
+
+JavaScript interprets (or unboxes) statements. When we use bracket notation, it sees the first opening bracket \`[\` and keeps going until it finds the closing bracket \`]\`. Only then, it will evaluate the statement.
+
+\`mouse[bird.size]\`: First it evaluates \`bird.size\`, which is \`"small"\`. \`mouse["small"]\` returns \`true\`
+
+However, with dot notation, this doesn't happen. \`mouse\` does not have a key called \`bird\`, which means that \`mouse.bird\` is \`undefined\`. Then, we ask for the \`size\` using dot notation: \`mouse.bird.size\`. Since \`mouse.bird\` is \`undefined\`, we're actually asking \`undefined.size\`. This isn't valid, and will throw an error similar to \`Cannot read property "size" of undefined\`.
+        `,
+      },
+      {
+        text: 'What`s the output?',
+        codeSnippet: `
+  \`\`\`js
+  let c = { greeting: 'Hey!' };
+  let d;
+  
+  d = c;
+  c.greeting = 'Hello';
+  console.log(d.greeting);
+  \`\`\`
+      `,
+        options: [
+          { text: 'Hello', isCorrect: true },
+          { text: 'Hey!', isCorrect: false },
+          { text: 'undefined', isCorrect: false },
+          { text: 'ReferenceError', isCorrect: false },
+          { text: 'TypeError', isCorrect: false },
+        ],
+        explanation: `
+        In JavaScript, all objects interact by _reference_ when setting them equal to each other.
+
+First, variable \`c\` holds a value to an object. Later, we assign \`d\` with the same reference that \`c\` has to the object.
+
+When you change one object, you change all of them.
+        `,
+      },
     ],
   },
 ];
